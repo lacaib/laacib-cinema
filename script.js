685e6a8ecb295dfd02a33c67dbfca6ed2af4319c
lacaib <nlaperto3@gmail.com>
2026-05-10 12:55:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,235 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { db, storage } from "./firebase.js";
 
 import {
-getFirestore
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
-getStorage
+ref,
+uploadBytes,
+getDownloadURL
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-const firebaseConfig = {
-apiKey: "AIzaSyBHXDHJq6iX_wUi3B9Ggbs_YMyIPCw6fpU",
-authDomain: "laacib-cinema.firebaseapp.com",
-projectId: "laacib-cinema",
-storageBucket: "laacib-cinema.firebasestorage.app",
-messagingSenderId: "504616052691",
-appId: "1:504616052691:web:1d432f3fa51a88f8217126"
+const ADMIN_PASSWORD = "IPhone33@foyj45@^";
+
+const moviesGrid = document.getElementById("moviesGrid");
+
+const deleteMoviesBox = document.getElementById("deleteMoviesBox");
+
+const googleBtn = document.getElementById("googleBtn");
+
+googleBtn.onclick = () => {
+
+let user = localStorage.getItem("laacibUser");
+
+if(!user){
+
+const name = prompt("Geli magacaaga");
+
+if(name){
+
+localStorage.setItem("laacibUser",name);
+
+googleBtn.innerHTML = name;
+
+}
+
+}else{
+
+googleBtn.innerHTML = user;
+
+}
+
 };
 
-const app = initializeApp(firebaseConfig);
+window.onload = ()=>{
 
-const db = getFirestore(app);
+loadMovies();
 
-const storage = getStorage(app);
+const user = localStorage.getItem("laacibUser");
 
-export { db, storage };
+if(user){
+
+googleBtn.innerHTML = user;
+
+}
+
+};
+
+async function loadMovies(){
+
+moviesGrid.innerHTML = "";
+
+deleteMoviesBox.innerHTML = "";
+
+const querySnapshot = await getDocs(collection(db,"movies"));
+
+querySnapshot.forEach((docSnap)=>{
+
+const movie = docSnap.data();
+
+const id = docSnap.id;
+
+moviesGrid.innerHTML += `
+
+<div class="movie-card">
+
+<img src="${movie.image}">
+
+<div class="movie-info">
+
+<h3>${movie.name}</h3>
+
+<p><b>Actors:</b> ${movie.actors}</p>
+
+<div class="stars">${movie.stars}</div>
+
+<p>${movie.desc}</p>
+
+<div class="actions">
+
+<button class="watch"
+onclick="window.open('${movie.video}')">
+Watch
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+deleteMoviesBox.innerHTML += `
+
+<div class="delete-item">
+
+${movie.name}
+
+<button onclick="deleteMovie('${id}')">
+Delete
+</button>
+
+</div>
+
+`;
+
+});
+
+}
+
+window.deleteMovie = async function(id){
+
+const ask = confirm("Delete this movie?");
+
+if(!ask) return;
+
+await deleteDoc(doc(db,"movies",id));
+
+alert("Movie Deleted");
+
+loadMovies();
+
+}
+
+window.addMovie = async function(){
+
+const name = document.getElementById("movieName").value;
+
+const actors = document.getElementById("movieActors").value;
+
+const stars = document.getElementById("movieStars").value;
+
+const desc = document.getElementById("movieDesc").value;
+
+const imageFile = document.getElementById("movieImage").files[0];
+
+const videoFile = document.getElementById("movieVideo").files[0];
+
+if(
+!name ||
+!imageFile ||
+!videoFile
+){
+
+alert("Fill all inputs");
+
+return;
+
+}
+
+try{
+
+alert("Uploading...");
+
+const imageRef = ref(
+storage,
+"images/" + Date.now() + imageFile.name
+);
+
+await uploadBytes(imageRef,imageFile);
+
+const imageURL = await getDownloadURL(imageRef);
+
+const videoRef = ref(
+storage,
+"videos/" + Date.now() + videoFile.name
+);
+
+await uploadBytes(videoRef,videoFile);
+
+const videoURL = await getDownloadURL(videoRef);
+
+await addDoc(collection(db,"movies"),{
+
+name,
+actors,
+stars,
+desc,
+image:imageURL,
+video:videoURL
+
+});
+
+alert("Movie Added Successfully");
+
+closeAdmin();
+
+loadMovies();
+
+}catch(err){
+
+console.log(err);
+
+alert("Upload Error");
+
+}
+
+}
+
+if(window.location.hash === "#admin"){
+
+const password = prompt("Enter Admin Password");
+
+if(password === ADMIN_PASSWORD){
+
+document.getElementById("adminPanel").style.display = "flex";
+
+}else{
+
+alert("Wrong Password");
+
+}
+
+}
+
+window.closeAdmin = function(){
+
+document.getElementById("adminPanel").style.display = "none";
+
+}
