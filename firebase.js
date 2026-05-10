8903fe0d1d11bcc3e27cdb26119fb8d66e35bbfb
lacaib <nlaperto3@gmail.com>
2026-05-10 13:25:23 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,29 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-  getFirestore
+getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithPopup,
-  signOut
+getAuth,
+GoogleAuthProvider,
+signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHXDHJq6iX_wUi3B9Ggbs_YMyIPCw6fpU",
-  authDomain: "laacib-cinema.firebaseapp.com",
-  projectId: "laacib-cinema",
-  storageBucket: "laacib-cinema.firebasestorage.app",
-  messagingSenderId: "504616052691",
-  appId: "1:504616052691:web:1d432f3fa51a88f8217126"
+
+apiKey: "AIzaSyBHXDHJq6iX_wUi3B9Ggbs_YMyIPCw6fpU",
+
+authDomain: "laacib-cinema.firebaseapp.com",
+
+projectId: "laacib-cinema",
+
+storageBucket: "laacib-cinema.firebasestorage.app",
+
+messagingSenderId: "504616052691",
+
+appId: "1:504616052691:web:1d432f3fa51a88f8217126"
+
 };
 
 const app = initializeApp(firebaseConfig);
@@ -29,9 +35,8 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export {
-  db,
-  auth,
-  provider,
-  signInWithPopup,
-  signOut
+db,
+auth,
+provider,
+signInWithPopup
 };
