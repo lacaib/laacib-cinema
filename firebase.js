d3170b4b6142fe0f01968bfa2f32a4f426233847
lacaib <nlaperto3@gmail.com>
2026-05-10 11:13:37 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,26 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
+import {
+getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+getStorage
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBHXDHJq6iX_wUi3B9Ggbs_YMyIPCw6fpU",
-  authDomain: "laacib-cinema.firebaseapp.com",
-  projectId: "laacib-cinema",
-  storageBucket: "laacib-cinema.firebasestorage.app",
-  messagingSenderId: "504616052691",
-  appId: "1:504616052691:web:1d432f3fa51a88f8217126"
+apiKey: "AIzaSyBHXDHJq6iX_wUi3B9Ggbs_YMyIPCw6fpU",
+authDomain: "laacib-cinema.firebaseapp.com",
+projectId: "laacib-cinema",
+storageBucket: "laacib-cinema.firebasestorage.app",
+messagingSenderId: "504616052691",
+appId: "1:504616052691:web:1d432f3fa51a88f8217126"
 };
 
 const app = initializeApp(firebaseConfig);
 
-console.log("Firebase Connected 🔥");
+const db = getFirestore(app);
+
+const storage = getStorage(app);
+
+export { db, storage };
